e9cbcdd70b264af9f7b3b672d6cc143bbd4a02c4
Iru-Brandon77 <brsandovalca@uide.edu.ec>
2026-06-19 01:30:12 +0000

    WIP: filter columns

--- blackhatbash.sh
+++ blackhatbash.sh
@@ -10,6 +10,10 @@ grep "35.237.4.214\|13.66.139.0" log.txt
 
 grep -v "35.237.4.214" log.txt
 
+grep -o "35.237.4.214" log.txt
 
+#imprimir la primera columna
+awk '{print $1}' log.txt
 
- grep -o "35.237.4.214" log.txt
+#imprimir la primera, segunda y tercera columna
+awk '{print $1,$2,$3}' log.txt
